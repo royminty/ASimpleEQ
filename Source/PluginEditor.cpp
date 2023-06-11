5e9ddef8364eaac0fd67c63c15d7c7da212cb04f
Roy Min <royminty12@gmail.com>
2023-06-11 17:15:54 -0400

    added analyzer db marks
    cleaned up code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -417,6 +417,15 @@ void ResponseCurveComponent::resized()
         g.setColour(gDb == 0.f ? Colour(186u, 186u, 186u) : Colours::lightgrey);
 
         g.drawFittedText(str, r, juce::Justification::centred, 1);
+
+        str.clear();
+        str << (gDb - 24.f);
+
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight);
+        g.setColour(Colours::lightgrey);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
     }
 }
 
